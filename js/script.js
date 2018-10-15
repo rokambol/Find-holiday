38a14a16cd586085c786c8fa5c5002fdf5a1542a
Dimitar Milev <rokambol_88@abv.bg>
2018-10-15 11:41:35 +0000

    fix problem with push

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 
-//$(document).ready(function(){ 
-       
-
-/*coords = {
-  africa: '39.953050,-75.163961',
-  europe: '40.875597,-77.776226',
-  asia: '41.763633,-72.682662'
-};   */ 
 var map;
-
+var childLi1="<li>Washington</li> <li>New York</li> <li>Toronto</li> <li>Monreal</li> <li>Quebec City</li> <li>Vancouver</li>";
+var childLi2="<li>Rio de Jenairo</li> <li>Buenos Aires</li> <li>Montivideo</li> <li>Falkland Islands</li> <li>Rio Grande</li> <li>Santiago</li>";
+var childLi3="<li>New York</li> <li>Montivideo</li>";
+ 
+var locations = [
+      ['Washington DC', 38.893558, -77.063383, 1],
+      ['New York', 40.773882, -74.019295, 2],
+      ['Toronto', 43.697069, -79.383891, 3],
+      ['Monreal', 45.605755, -73.538326, 4],
+      ['Quebec City', 46.867889, -71.226519, 5],
+      ['Vancouver', 49.384010, -123.156729, 6],
+      ['Rio de Jenairo', -22.922876, -43.462457, 7],
+      ['Buenos Aires', -34.604760, -58.426976, 8],
+      ['Montivideo', -34.902597, -56.172354, 9],
+      ['Falkland Islands (Islas Malvinas)', -51.746976, -58.910415, 10],
+      ['Rio Grande', -53.763299, -67.696776, 11],
+      ['Santiago', -33.439446, -70.667641, 12],
+      [],
+      [],
+      [],
+    ];
+//var iternityNY = {lat: -25.344, lng: 131.036};
+//var marker =  new google.maps.Marker({position: iternityNY, map: map});
 
 /*initial load default map center when page is loaded */
       function initMap() {
@@ -16,6 +30,24 @@ var map;
           center: {lat: -34.397, lng: 150.644},
           zoom: 3
         });
+        var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        };
+      })
+      (marker, i));
+    }
     }
     
     
@@ -34,11 +66,14 @@ $(document).ready(function() {
  var value =  $('#inlineFormCustomSelect').val();
   
   if (value == 1) {
-       newLocation(49.020956,-103.076235),listNY(),listT(),listCity();
+       newLocation(49.020956,-103.076235);
+        $('li').replaceWith(childLi1);
 } else if (value == 2) {
-     newLocation(-13.777778, -57.812563);
+     newLocation(-33.139444, -65.501989);
+     $('li').replaceWith(childLi2);
 } else if (value == 3) {
     newLocation(50.720127, 16.191343);
+     $('li').replaceWith(childLi3);
 } else if (value == 4) {
     newLocation(3.575398, 23.046812);
 } else if (value == 5) {
@@ -50,44 +85,19 @@ $(document).ready(function() {
 }
 });
 
+
+/*$('#inlineFormCustomSelect').on('change', function (){
+var value =  $('#inlineFormCustomSelect').val();
+if (value == 1 ) {
+
+    $('li').replaceWith(childLi1);
+    
+} else if(value == 2) {
  
- function listNY() {
-    var newItem = document.createElement('li');
-    var textnode = document.createTextNode("New York");
-    newItem.appendChild(textnode);
-
-    var list = document.getElementById("list1");
-    list.insertBefore(newItem, list.childNodes[0]);
-   for( i = 0; i < 1; i = i + 1 ){
-    var li = document.createElement( 'li' );               // create a new li element
-    li.textContent = "Vancouver " + (i+1);                                                  // .textContent is mush shorter than creating a textNode
-    list1.appendChild( li );                           // every time append a new item
-  }
+    $('li').replaceWith(childLi2);
 }
-function listT() {
-    var newItem = document.createElement('li');
-    var textnode = document.createTextNode("Washington");
-    newItem.appendChild(textnode);
 
-    var list = document.getElementById("list1");
-    list.insertBefore(newItem, list.childNodes[0]);
-    for( i = 1; i < 2; i = i + 1 ){
-    var li = document.createElement( 'li' );               // create a new li element
-    li.textContent = "Tenessi " + (i+1);                                                  // .textContent is mush shorter than creating a textNode
-    list1.appendChild( li );                           // every time append a new item
-  }
-}   
-function listCity() {
-    var newItem = document.createElement('li');
-    var textnode = document.createTextNode("Las Vegas");
-    newItem.appendChild(textnode);
 
-    var list = document.getElementById("list1");
-    list.insertBefore(newItem, list.childNodes[0]);
-    for( i = 1; i < 2; i = i + 1 ){
-    var li = document.createElement( 'li' );               // create a new li element
-    li.textContent = "Quebec" + (i+1);                                                  // .textContent is mush shorter than creating a textNode
-    list1.appendChild( li );                           // every time append a new item
-  }
-}   
+
+});*/
 });
